71fb08cf845ce7d48d361311a8b0777e23c66532
Jihun-Go <gn753@naver.com>
2022-03-01 02:40:58 +0900

    typescript 웹팩셋팅 반은 실패

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,25 @@
-const path = require("path");
-const webpack = require("webpack");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+import path from "path";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import type { Configuration } from "webpack";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin"; // ts 변환 속도 향상
 // 핫리로딩 플러그인
-
-module.exports = {
+const devServer: DevServerConfiguration = {};
+const config: Configuration = {
   name: "wordrelady-setting",
   mode: "development",
   devtool: "eval",
   resolve: {
-    extensions: [".js", ".jsx", ".json"], //확장자 이름
+    extensions: ["tsx", "ts", "json", "js", "jsx"], //확장자 이름
   },
 
   module: {
     rules: [
       // 규칙
       {
-        test: /\.jsx/,
+        test: /\.tsx/,
+        use: "ts-loader",
+        exclude: /node_modules/,
         loader: "babel-loader",
         options: {
           presets: [
@@ -43,7 +47,7 @@ module.exports = {
     app: "./client",
     //최상위 루트.js하나만 경로로 설정 시 나머지는 같이 끌려온다.
   },
-  plugins: [new ReactRefreshWebpackPlugin()],
+  plugins: [new ReactRefreshWebpackPlugin(), new ForkTsCheckerWebpackPlugin()],
   output: {
     path: path.join(__dirname, "dist"), // 특정 경로로 파일을 모아준다.
     filename: "app.js", // 합쳐서 만들어준것, 결과물의 이름
@@ -51,7 +55,9 @@ module.exports = {
   },
   devServer: {
     devMiddleware: { publicPath: "/dist/" },
-    static: { directory: path.resolve(__dirname) },
+    static: { directory: path.join(__dirname) }, //절대경로
     hot: true,
   },
 };
+
+export default config;
